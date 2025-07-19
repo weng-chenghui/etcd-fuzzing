e95242935195ca48ce5c32c04541abb4bb8b818c
Chenghui Weng <144981080+weng-chenghui@users.noreply.github.com>
2025-07-19 11:11:50 +0900

    Fix the div zero error when running with "fuzz"

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ var (
 	requests     int
 	numRuns      int
 	recordTraces bool
+	host         string
+	port         int
+	address      string
 )
 
 func main() {
@@ -25,6 +28,8 @@ func main() {
 	rootCommand.PersistentFlags().IntVar(&requests, "requests", 1, "Num of initial requests to serve")
 	rootCommand.PersistentFlags().IntVar(&numRuns, "runs", 5, "Number of runs to average over")
 	rootCommand.PersistentFlags().BoolVar(&recordTraces, "record-traces", false, "Record the traces explored")
+	rootCommand.PersistentFlags().StringVar(&host, "host", "127.0.0.1", "Host address to use")
+	rootCommand.PersistentFlags().IntVar(&port, "port", 2023, "Port to use")
 	rootCommand.AddCommand(FuzzCommand())
 	rootCommand.AddCommand(OneCommand())
 
@@ -37,11 +42,12 @@ func FuzzCommand() *cobra.Command {
 	return &cobra.Command{
 		Use: "fuzz",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			address = fmt.Sprintf("%s:%d", host, port)
 			fuzzer := NewFuzzer(&FuzzerConfig{
 				Iterations: episodes,
 				Steps:      horizon,
 				Strategy:   NewRandomStrategy(),
-				Guider:     NewLineCoverageGuider("127.0.0.1:2023", "traces", recordTraces),
+				Guider:     NewLineCoverageGuider(address, "traces", recordTraces),
 				Mutator:    &EmptyMutator{},
 				RaftEnvironmentConfig: RaftEnvironmentConfig{
 					Replicas:      replicas,
@@ -54,6 +60,8 @@ func FuzzCommand() *cobra.Command {
 				CrashQuota:         2,
 				MaxMessages:        10,
 				SeedPopulationSize: 10,
+				// Must specify a value otherwise it throws the div zero error.
+				ReseedFrequency: 2000,
 			})
 			fuzzer.Run()
 			return nil
@@ -65,7 +73,7 @@ func OneCommand() *cobra.Command {
 	return &cobra.Command{
 		Use: "compare",
 		Run: func(cmd *cobra.Command, args []string) {
-
+			address = fmt.Sprintf("%s:%d", host, port)
 			c := NewComparision(savePath, &FuzzerConfig{
 				Iterations: episodes,
 				Steps:      horizon,
@@ -91,10 +99,10 @@ func OneCommand() *cobra.Command {
 				ReseedFrequency:    2000,
 			}, numRuns)
 			combinedMutator := CombineMutators(NewSwapCrashNodeMutator(2), NewSwapNodeMutator(20), NewSwapMaxMessagesMutator(20))
-			c.Add("traceCov", combinedMutator, NewTraceCoverageGuider("127.0.0.1:2023", "traces", recordTraces))
-			c.Add("lineCov", combinedMutator, NewLineCoverageGuider("127.0.0.1:2023", "traces", recordTraces))
-			c.Add("tlcstate", combinedMutator, NewTLCStateGuider("127.0.0.1:2023", "traces", recordTraces))
-			c.Add("random", &EmptyMutator{}, NewTLCStateGuider("127.0.0.1:2023", "traces", recordTraces))
+			c.Add("traceCov", combinedMutator, NewTraceCoverageGuider(address, "traces", recordTraces))
+			c.Add("lineCov", combinedMutator, NewLineCoverageGuider(address, "traces", recordTraces))
+			c.Add("tlcstate", combinedMutator, NewTLCStateGuider(address, "traces", recordTraces))
+			c.Add("random", &EmptyMutator{}, NewTLCStateGuider(address, "traces", recordTraces))
 
 			c.Run()
 		},
